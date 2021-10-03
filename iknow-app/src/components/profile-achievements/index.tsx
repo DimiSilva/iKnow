fa0572d7f027f8f4edc98e8f97a06ac4eb6227bf
Dimi Silva <jnn.sla@gmail.com>
2021-10-03 02:46:01 -0300

    iknow-app: sort achievements by tier

--- iknow-app/src/components/profile-achievements/index.tsx
+++ iknow-app/src/components/profile-achievements/index.tsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import iknowCommon from 'iknow-common'
 import { Collapse, Achievement } from '../index'
 import IComponentProps from './interfaces/i-component-props'
 import './style.scss'
+
+const tiersOrder = {
+    [iknowCommon.enums.achievementTiers.WOOD]: 1,
+    [iknowCommon.enums.achievementTiers.BRONZE]: 2,
+    [iknowCommon.enums.achievementTiers.SILVER]: 3,
+    [iknowCommon.enums.achievementTiers.GOLD]: 4,
+    [iknowCommon.enums.achievementTiers.DIAMOND]: 5,
+}
 
 const ProfileAchievements: React.FC<IComponentProps> = ({ achievements }) => (
     <div className="profile-achievements">
@@ -17,7 +26,11 @@ Nessa sessão você pode as conquistas, conquistas podem ser ganhas realizando d
                 ? (
                     <div className="profile-achievements-achievements">
                         {
-                            achievements.map((achievement) => (
+                            achievements.sort((a, b) => {
+                                if (tiersOrder[a.tier] > tiersOrder[b.tier]) return 1
+                                if (tiersOrder[a.tier] < tiersOrder[b.tier]) return -1
+                                return 0
+                            }).map((achievement) => (
                                 <div key={achievement.key + achievement.tier}>
                                     <Achievement
                                         _key={achievement.key}
